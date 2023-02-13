04b548c5c91abf3a0ab3969437120a54f1274246
AroraSanya <sanya.arora@netsolutions.com>
2023-02-13 17:48:20 +0530

    Assignment of 13 Feb

--- 11_feb/convert_in_words.py
+++ 11_feb/convert_in_words.py
@@ -9,35 +9,33 @@ def format_input_amount(amount):
     This function is used to format the amount
     ex :25
     Rs: 25.00
-    :param amount
-    :return
+    :param amountz
     """
     return f"{amount}.00"
 
-def convert_to_words(func):
-    def inner(amt):  
+def convert_to_words(func,amt):
         for_amount = func(amt)  # function uses another function 
         n = int(float(for_amount))
         print(type(n))
         if n>99999:   
              print("Cant solve for more than 5 digits")
         else:   
-            digit=[0,0,0,0,0]                   # input the amount digit in a list 
+            digit=[0,0,0,0,0]                    
             i=0
             while n>0:
                 digit[i]=n%10
                 i+=1
                 n=n//10                         # input the number in list d []
             num=""
-            if digit[4]!=0:                      # ex : 3456 d =[6,5,4,3]
-                if(digit[4]==1):                 #  # Last element of list
+            if digit[4]!=0:                      # ex : 34567 d =[7,6,5,4,3]
+                if(digit[4]==1):                 
                     num+=Tens[digit[3]]+ " Thousand "    # third element of list
                 else:
                     num+=nty[digit[4]]+" "+Ones[digit[3]]+  " Thousand "    #combining the last and third words of list 
             else:
                 if digit[3]!=0:
                     num+=Ones[digit[3]]+ " Thousand " # if last element is zero
-            if digit[2]!=0:
+            if digit[2]!=0:                                          
                 num+=Ones[digit[2]]+" Hundred "
             if digit[1] != 0:                          # for first 2 digits for d list
                 if (digit[1] == 1):
@@ -49,7 +47,6 @@ def convert_to_words(func):
                     num += Ones[digit[0]]
 
         return num   
-    return inner
 
-div = convert_to_words(format_input_amount)
-print(div(amount))
+div = convert_to_words(format_input_amount,amount)
+print(div)
