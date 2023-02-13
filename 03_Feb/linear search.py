2bc3eecbfa741cf714a47790dd49685d615a13c8
AroraSanya <sanya.arora@netsolutions.com>
2023-02-13 18:02:39 +0530

    linear assignment

--- 03_Feb/linear search.py
+++ 03_Feb/linear search.py
@@ -1,11 +1,15 @@
 def search(list,n):
     for i in range(len(list)):
-        if list[i] ==  n:
-            print("Number is present at ",i) 
-    return print("Number is  not present ") 
+        if list[i] ==  n:  
+            return i 
+    return -1
                 
 
 
 list=[2,4,55,6]
 n=int(input("enter the number:"))
-search(list,n)   
+res = search(list,n) 
+if res == -1:
+    print("not found")  
+else:
+    print(res)
